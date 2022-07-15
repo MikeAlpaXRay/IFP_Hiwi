7dfc6fee29c517cd22bb57cee7313603ba101289
MikeAlpaXRay <fTfVgMVTE|+kB@mE[{m>
2022-07-15 14:44:58 +0200

    path fix

---
+++ 7_Surface Matching/Surface_Matching_Package/matching.bat
@@ -0,0 +1,6 @@
+:: Activates Python Enviroment
+call conda activate OpenCVSurfaceMatching
+:: Change to personal Filepath
+python "E:\Cloud\OneDrive - bwedu\Studium\Master\HiWi\IFP\7_Surface Matching\Surface_Matching_Package\ppf_load_match.py"
+call conda deactivate
+pause
